b79b1630cdbd78c17f2eda1aac87c06f91f011b8
dezrenmikell <dezren.mikell@yahoo.com>
2020-03-27 23:12:50 -0400

    created models

--- juice-django-react/jl_app/models.py
+++ juice-django-react/jl_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+    description = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.name
+
+class Poem(models.Model):
+    title = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+    preview_url = models.CharField(max_length=400)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='poems')
+
+    def __str__(self):
+        return self.title
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=800)
+    preview_url = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.title
+
+class Picture(models.Model):
+    title = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+    
+    def __str__(self):
+        return self.title
